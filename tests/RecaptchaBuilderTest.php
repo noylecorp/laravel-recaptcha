93269607ab003621db7e5160c6b3cd1eb94bf7a6
vagrant <nsboyle@gmail.com>
2015-09-16 19:13:22 +0000

    Update testing and add a couple more

--- tests/RecaptchaBuilderTest.php
+++ tests/RecaptchaBuilderTest.php
@@ -16,14 +16,19 @@ class RecaptchaBuilderTest extends PHPUnit_Framework_TestCase
     {
         $this->urlGenerator = new UrlGenerator(new RouteCollection(), Request::create('/foo', 'GET'));
         $this->htmlBuilder = new HtmlBuilder($this->urlGenerator);
-        $this->recaptchaBuilder = new RecaptchaBuilder($this->htmlBuilder);
+        $this->siteKey = 'my-site-key';
+        $this->recaptchaBuilder = new RecaptchaBuilder($this->siteKey, $this->htmlBuilder);
     }
 
     public function testWidget()
     {
-        $r1 = $this->recaptchaBuilder->widget('my-site-key');
+        $r1 = $this->recaptchaBuilder->widget();
+        $r2 = $this->recaptchaBuilder->widget(['theme' => 'dark']);
+        $r3 = $this->recaptchaBuilder->widget(['type' => 'audio']);
 
         $this->assertEquals('<div class="g-recaptcha" data-sitekey="my-site-key"></div>'.PHP_EOL, $r1);
+        $this->assertEquals('<div class="g-recaptcha" data-theme="dark" data-sitekey="my-site-key"></div>'.PHP_EOL, $r2);
+        $this->assertEquals('<div class="g-recaptcha" data-type="audio" data-sitekey="my-site-key"></div>'.PHP_EOL, $r3);
     }
 
     public function testScript()
