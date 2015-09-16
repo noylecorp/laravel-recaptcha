a188a11f967e954089af9dd6db31228f4c2fb29f
Nick Boyle <nsboyle@gmail.com>
2015-09-16 13:43:14 -0400

    Reworking attribute renaming to work only on actual config params

--- src/RecaptchaBuilder.php
+++ src/RecaptchaBuilder.php
@@ -7,31 +7,32 @@ use Collective\Html\HtmlBuilder;
 class RecaptchaBuilder
 {
     protected $html;
-    
+
+    protected $tagAttributes = ['sitekey', 'theme', 'type', 'size', 'tabindex', 'callback', 'expired-callback'];
+
     public function __construct(HtmlBuilder $html)
     {
         $this->html = $html;
     }
-    
+
     public function render($sitekey, array $options = [])
     {
         if (!isset($options['sitekey'])) {
             $options['sitekey'] = $sitekey;
         }
-        
-        $options = $this->renameOptions($options);
-        
+
+        $this->renameOptions($options);
+
         return '<div class="g-recaptcha"'.$this->html->attributes($options).'></div>';
     }
-    
-    public function renameOptions(array $options)
+
+    public function renameOptions(array &$options)
     {
-        $renamed = [];
-        
         foreach($options as $k => $v) {
-            $renamed['data-'.$k] = $v;
+            if (in_array($k, $this->tagAttributes)) {
+                $options['data-'.$k] = $v;
+                unset($options[$k]);
+            }
         }
-        
-        return $renamed;
     }
 }
