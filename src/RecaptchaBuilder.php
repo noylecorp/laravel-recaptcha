8fb0b7c8d87984250ae8e509e0029b67472e314d
Nick Boyle <nsboyle@gmail.com>
2015-09-16 13:11:56 -0400

    Create RecaptchaBuilder.php

---
+++ src/RecaptchaBuilder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Noylecorp\Recaptcha;
+
+use Collective\Html\HtmlBuilder;
+
+class RecaptchaBuilder
+{
+    protected $html;
+    
+    public function __construct(HtmlBuilder $html)
+    {
+        $this->html = $html
+    }
+    
+    public function render($sitekey, array $options = [])
+    {
+        if (!isset($options['sitekey'])) {
+            $options['sitekey'] = $sitekey;
+        }
+        
+        $options = $this->renameOptions($options);
+        
+        return '<script src='https://www.google.com/recaptcha/api.js'></script>'
+              .'<div class="g-recaptcha" '.$this->html->attributes($options).'></div>'
+        ;
+    }
+    
+    public function renameOptions(array $options)
+    {
+        $renamed = [];
+        
+        foreach($options as $k, $v) {
+            $renamed['data-'.$k] = $v;
+        }
+        
+        return $renamed;
+    }
+}
