d7f7778f82436bff39421d9106454095184559a1
vagrant <nsboyle@gmail.com>
2015-09-17 13:55:25 +0000

    Adding code docblocks

--- src/RecaptchaBuilder.php
+++ src/RecaptchaBuilder.php
@@ -6,20 +6,55 @@ use Collective\Html\HtmlBuilder;
 
 class RecaptchaBuilder
 {
+    /**
+     * The HTML builder instance.
+     *
+     * @var \Collective\Html\HtmlBuilder
+     */
     protected $html;
 
+    /**
+     * The reCAPTCHA widget class
+     *
+     * @var string
+     */
     protected $recaptchaClass = 'g-recaptcha';
 
+    /**
+     * The reCAPTCHA site key
+     *
+     * @var string
+     */
     protected $siteKey;
 
+    /**
+     * Tag attributes recognized as configuration options by reCAPTCHA
+     *
+     * @var array
+     */
     protected $tagAttributes = ['sitekey', 'theme', 'type', 'size', 'tabindex', 'callback', 'expired-callback'];
 
+    /**
+     * Create a new reCAPTCHA builder instance.
+     *
+     * @param  string                           $siteKey
+     * @param  \Collective\Html\HtmlBuilder     $html
+     *
+     * @return void
+     */
     public function __construct($siteKey, HtmlBuilder $html)
     {
         $this->html = $html;
         $this->siteKey = $siteKey;
     }
 
+    /**
+     * Create <div> tag for a reCAPTCHA widget
+     *
+     * @param  array $options
+     *
+     * @return string
+     */
     public function widget(array $options = [])
     {
         $options['sitekey'] = $this->siteKey;
@@ -36,16 +71,37 @@ class RecaptchaBuilder
         return '<div'.$this->html->attributes($options).'></div>'.PHP_EOL;
     }
 
+    /**
+     * Create <script> and <div> tags for a reCAPTCHA widget
+     *
+     * @param  array $options
+     *
+     * @return string
+     */
     public function recaptcha(array $options = [])
     {
         return $this->script().$this->widget($options);
     }
 
+    /**
+     * Create <script> tag for a reCAPTCHA widget
+     *
+     * @param  array $options
+     *
+     * @return string
+     */
     public function script()
     {
         return $this->html->script('https://www.google.com/recaptcha/api.js');
     }
 
+    /**
+     * Rename specified options keys to attribute names recognized by reCAPTCHA
+     *
+     * @param  array &$options
+     *
+     * @return string
+     */
     public function renameOptions(array &$options)
     {
         foreach($options as $k => $v) {
--- src/RecaptchaVerifier.php
+++ src/RecaptchaVerifier.php
@@ -4,13 +4,33 @@ namespace Noylecorp\Recaptcha;
 
 class RecaptchaVerifier
 {
+    /**
+     * The reCAPTCHA secret key
+     *
+     * @var string
+     */
     protected $secretKey;
 
+    /**
+     * Create a new reCAPTCHA verifier instance.
+     *
+     * @param  string                           $secretKey
+     * @param  \Collective\Html\HtmlBuilder     $html
+     *
+     * @return void
+     */
     public function __construct($secretKey)
     {
         $this->secretKey = $secretKey;
     }
 
+    /**
+     * Verify a reCAPTCHA response
+     *
+     * @param  string                           $response
+     *
+     * @return bool
+     */
     public function verify($response)
     {
         $payload = [
